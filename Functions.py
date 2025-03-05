f3a08676780de1ff7de7ad4db321cf4f52667b5d
Robert Erdei <91911762+RobErdei@users.noreply.github.com>
2025-03-05 18:01:06 -0500

    Updated liked songs query

--- Functions.py
+++ Functions.py
@@ -11,115 +11,59 @@ def GetPlaylistInfo_many(token_info):
     sp = spotipy.Spotify(auth=token_info)
     current_playlists = sp.current_user_playlists()['items']
 
-    #Playlist Info table
-    df1 = pd.DataFrame({
-        "Playlist ID": [],
-        "Playlist Name": [],
-        "Link": [],
-        "Owner": [],
-        "Owner Type": [],
-        "Track Count": [],
-        "Object Type": []
-    })
+    playIDs = []
 
-    #Genral Song Info table
-    df2 = pd.DataFrame({
-        "Playlist ID": [],
-        "Object Type": [],
-        "Album Id": [],
-        "Album Name": [],
-        "Album Release Date ": [],
-        "Album Artist Id": [],
-        "Album Artist Name": [],
-        "Album Object Type": [],
-        "Album Total Tracks": [],
-        "Other Object Type": [],
-        "Album Uri": [],
-        "Song Number In Album": [],
-        "Song Id": [],
-        "Song Name": [],
-        "Song Popularity": [],
-        "Song Type": [],
-        "Song Duration": [],
-        "Song Explicit": [],
-        "Song Uri": []
-    })
-
-    #Artist Info Table
-    df3 = pd.DataFrame({
-        "ID":[], #KEY
-        "Name": [],
-        "Genres": []
-    })
-
-    #Song Features
-    df4 = pd.DataFrame({
-
-    })
-    
-    print("There are currently " + str(len(current_playlists)) + " playlists\n")
-    for i in current_playlists: #Appends specified fields to df1 (the "Playlist Info" table)
+    for i in current_playlists: # Get overview playlist data
         play_id = i["id"]
+        playIDs.append(play_id)
         play_name = i["name"]
         link = i["href"]
         owner = i["owner"]["display_name"]
         owner_type = i["owner"]["type"]
         track_count = i["tracks"]["total"]
         obj_type = i["type"]
-        df1.loc[len(df1.index)] = [play_id, play_name, link, owner, owner_type, track_count, obj_type]
+        newPlayRow = [play_id, play_name, link, owner, owner_type, track_count, obj_type]
 
-    totalLength = sum([i["tracks"]["total"] for i in current_playlists])
+    def parsePlaylistsJSON():
+        for row in playIDs:
+            content = sp.playlist_items(row)["items"]
 
-    curCount = 0
-    for row in df1["Playlist ID"]:
-        content = sp.playlist_items(row)["items"]
+            for song in content:
+                #Album Info
+                object_type = song["track"]["album"]["album_type"]
+                album_id = song["track"]["album"]['id']
+                album_name = song["track"]["album"]['name']
+                album_release_date = song["track"]["album"]["release_date"]
+                album_artist_id = song["track"]["album"]["artists"][0]["id"]
+                album_artist_name = song["track"]["album"]["artists"][0]["name"]
+                album_object_type = song["track"]["album"]["artists"][0]["type"]
+                album_total_tracks = song["track"]["album"]["total_tracks"]
+                other_object_type = song["track"]["album"]["type"]
+                album_uri = song["track"]["album"]["uri"]
+                song_number_in_album = song["track"]["track_number"]
+                
+                #Artist data (to be mapped afterwards through merge)
+                for artist in song["track"]["artists"]:
+                    track_artist_id = artist["id"]
+                    track_artist_name = artist["name"]
+                    track_artist_genre = ','.join([i for i in sp.artist(track_artist_id)['genres']])
+                    newArtistRow = F"('{track_artist_id}', '{track_artist_name}', '{track_artist_genre}')\n"
+                    yield newArtistRow
+                                    
+                #Song/Track Info
+                song_id = song["track"]["id"]
+                song_name = song["track"]["name"]
+                song_popularity = song["track"]["popularity"]
+                song_type = song["track"]["type"]
+                song_duration = int(song["track"]["duration_ms"]*(1/1000))    #converts song duration from milisecond to seconds
+                song_explicit =song["track"]["explicit"]
+                song_uri = song["track"]["uri"]
+                newSongRow = F"('{row}', '{object_type}', '{album_id}', '{album_name}', '{album_release_date}', '{album_artist_id}', '{album_artist_name}', '{album_object_type}', '{album_total_tracks}', '{other_object_type}', '{album_uri}', '{song_number_in_album}', '{song_id}', '{song_name}', '{song_popularity}', '{song_type}', '{song_duration}', '{song_explicit}', '{song_uri}')\n" 
+                
+                yield newSongRow
 
-        songCount = 0
-        for song in content:
-            #Album Info
-            object_type = song["track"]["album"]["album_type"]
-            album_id = song["track"]["album"]['id']
-            album_name = song["track"]["album"]['name']
-            album_release_date = song["track"]["album"]["release_date"]
-            album_artist_id = song["track"]["album"]["artists"][0]["id"]
-            album_artist_name = song["track"]["album"]["artists"][0]["name"]
-            album_object_type = song["track"]["album"]["artists"][0]["type"]
-            album_total_tracks = song["track"]["album"]["total_tracks"]
-            other_object_type = song["track"]["album"]["type"]
-            album_uri = song["track"]["album"]["uri"]
-            song_number_in_album = song["track"]["track_number"]
-            
-            #Artist data (to be mapped afterwards through merge)
-            for artist in song["track"]["artists"]:
-                track_artist_id = artist["id"]
-                track_artist_name = artist["name"]
-                track_artist_genre = ','.join([i for i in sp.artist(track_artist_id)['genres']])
-                df3.loc[len(df3.index)] = [track_artist_id, track_artist_name, track_artist_genre]
-                                
-            #Song/Track Info
-            song_id = song["track"]["id"]
-            song_name = song["track"]["name"]
-            song_popularity = song["track"]["popularity"]
-            song_type = song["track"]["type"]
-            song_duration = int(song["track"]["duration_ms"]*(1/1000))    #converts song duration from milisecond to seconds
-            song_explicit =song["track"]["explicit"]
-            song_uri = song["track"]["uri"]
-            new_row = [row, object_type, album_id, album_name, album_release_date, album_artist_id, album_artist_name, album_object_type, album_total_tracks, other_object_type, album_uri, song_number_in_album, song_id, song_name, song_popularity, song_type, song_duration, song_explicit, song_uri]
-            df2.loc[len(df2.index)] = new_row    
-            curCount += 1
+    return Response(stream_with_context(parsePlaylistsJSON()), content_type='text/plain')
 
-            totalPercent = (curCount / totalLength) * 100
-
-            bar = 'H' * int(totalPercent) + '-' * (100 - int(totalPercent))
-            bar_encoded = bar.encode('utf-8')
-            #print(f"\r|{bar}| {totalPercent:.2f}%", end="\r")
-        break
-
-    df3.drop_duplicates(subset=['ID'], keep=False) # Ensures artist ID field is unique in Artist table
-    PlaylistInfo_1 = pd.merge(df1,df2, on='Playlist ID', how='right') # All from second, matching from first
-    PlaylistInfo = pd.merge(PlaylistInfo_1,df3[['ID','Genres']], left_on='Album Artist Id', right_on='ID', how='left') # Merge on ID, include only Genres field from other table
-
-    return PlaylistInfo.to_html()
 
 def GetPlaylistInfo_single(token_info, playlist, owner):
     sp = spotipy.Spotify(auth=token_info)
@@ -174,7 +118,6 @@ def GetPlaylistInfo_single(token_info, playlist, owner):
     return Response(stream_with_context(JsonParsing()), content_type='text/plain')
 
 
-
 def GetPlaylistGenres(token_info, playlist, owner): # Get playlist genres
     sp = spotipy.Spotify(auth=token_info)
     result = sp.search(playlist, type='playlist')
@@ -204,80 +147,62 @@ def GetPlaylistGenres(token_info, playlist, owner): # Get playlist genres
 
     return frame.to_html(index=False)
 
-def GetLikedSongsInfo(token_info): # Gets LikedSongs info and exports to tabular/csv output
-    
-    df_song = pd.DataFrame({
-        "Artist ID": [],
-        "Artist Name": [],
-        "Artist URI": [],
-        "Song ID": [],
-        "Song Name": [],
-        "Song Popularity":[],
-        "Song URI": [],
-        "Added On":[],
-        "Song Artist Followers": [],
-        "Song Artist Genre": []
-    })
 
-    df_album = pd.DataFrame({
-        "Album Type": [],
-        "Album Artist Name": [],
-        "Album Artist ID": [],
-        "Album ID": [],
-        "Album Name": [],
-        "Album Release Date": [],
-        "Album URI": []
-    })
-
+def GetLikedSongsInfo(token_info):
     sp = spotipy.Spotify(auth=token_info)
 
-    batchSize = 20
-    desiredTotal = 500  # Edit as needed  
-    leftOver = desiredTotal % batchSize
+    batchSize = 20  # Max amount of songs API can pull for at once
+    desiredTotal = 10000  # Limit for liked songs that can be saved to spotify users' account
 
-    iterations = (desiredTotal // batchSize) + (1 if leftOver != 0 else 0)
-    iteration = 0
-    
-    for i in range(iterations):
-        fromVal = batchSize * iteration
-        toVal = desiredTotal - (desiredTotal-((iteration+1)*batchSize))
+    iterations = (desiredTotal // batchSize) + (1 if desiredTotal % batchSize != 0 else 0)
 
-        if desiredTotal < batchSize:
+    def parseBatchesJSON():
+        for i in range(iterations):
             fromVal = i * batchSize
-            toVal = fromVal + leftOver
-            batchSize = desiredTotal
+            currentBatchSize = min(batchSize, desiredTotal - fromVal)  # Handle last batch size
 
-        likedSongs = sp.current_user_saved_tracks(limit=batchSize,offset=fromVal)['items']
-        for i in likedSongs:
-            for artist in i['track']['album']['artists']:   #for album table
-                album_type = i['track']['album']['album_type']
-                artist_name = artist['name']
-                artist_id = artist['id']
-                album_id = i['track']['album']['id']
-                album_name = i['track']['album']['name']
-                album_release_date = i['track']['album']['release_date']
-                album_uri = i['track']['album']['uri']
-                df_album.loc[len(df_album.index)] = [album_type, artist_name, artist_id, album_id, album_name, album_release_date, album_uri]
+            likedSongs = sp.current_user_saved_tracks(limit=currentBatchSize, offset=fromVal)['items']
             
-            for artist in i['track']['artists']:            #for song table
-                song_artist_id = artist['id']
-                song_artist_name = artist['name']
-                song_artist_uri = artist['uri']
-                song_id = i['track']['id']
-                song_name = i['track']['name']
-                song_popularity = i['track']['popularity']
-                song_uri = i['track']['uri']
-                saved_on_DateTime = (i["added_at"][:10])+ " " +(i["added_at"][10:])
+            # Collect artist IDs in batch for optimization
+            artist_ids = []
+            for song in likedSongs:
+                for artist in song['track']['artists']:
+                    artist_ids.append(artist['id'])
 
-                song_artist_followers = str(sp.artist(song_artist_id)['followers']['total'])
-                song_artist_genres = ','.join([i for i in sp.artist(song_artist_id)['genres']])
+            artist_details = {artist["id"]: artist for artist in sp.artists(artist_ids)["artists"]}
 
-                df_song.loc[len(df_album.index)] = [song_artist_id, song_artist_name, song_artist_uri, song_id, song_name, song_popularity, song_uri, saved_on_DateTime, song_artist_followers, song_artist_genres]
-            
-        desiredTotal -= batchSize
-        iteration += 1
-    
-    return df_song.to_html(index=False)
+            for song in likedSongs:
+                for artist in song['track']['album']['artists']:  # For album table
+                    album_type = song['track']['album']['album_type']
+                    artist_name = artist['name']
+                    artist_id = artist['id']
+                    album_id = song['track']['album']['id']
+                    album_name = song['track']['album']['name']
+                    album_release_date = song['track']['album']['release_date']
+                    album_uri = song['track']['album']['uri']
+                    newArtistAlbumRow = f"('{album_type}', '{artist_name}', '{artist_id}', '{album_id}', '{album_name}', '{album_release_date}', '{album_uri}')\n"
+                    yield newArtistAlbumRow
+                
+                for artist in song['track']['artists']:  # For song table
+                    song_artist_id = artist['id']
+                    song_artist_name = artist['name']
+                    song_artist_uri = artist['uri']
+                    song_id = song['track']['id']
+                    song_name = song['track']['name']
+                    song_popularity = song['track']['popularity']
+                    song_uri = song['track']['uri']
+                    saved_on_DateTime = song["added_at"][:10] + " " + song["added_at"][10:]
+
+                    # Use pre-fetched artist details
+                    artist_info = artist_details.get(song_artist_id, {})
+                    song_artist_followers = str(artist_info.get('followers', {}).get('total', 0))
+                    song_artist_genres = ','.join(artist_info.get('genres', []))
+
+                    newArtistSongRow = f"('{song_artist_id}', '{song_artist_name}', '{song_artist_uri}', '{song_id}', '{song_name}', '{song_popularity}', '{song_uri}', '{saved_on_DateTime}', '{song_artist_followers}', '{song_artist_genres}')\n"
+                    yield newArtistSongRow
+
+    return Response(stream_with_context(parseBatchesJSON()), content_type='text/plain')
+
 
 def ArtistSearchQuery(token, art):  # Get Artist Genres
     sp = token
